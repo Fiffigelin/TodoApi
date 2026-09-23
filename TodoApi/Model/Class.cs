c16edc8c86f44b9059438614b4cf3ae11487b5de
Elin Nyman <elin.nyman87@gmail.com>
2026-09-23 16:20:14 +0200

    Create model, dbcontext and controller

---
+++ TodoApi/Model/Class.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Model;
+
+public class TodoItem
+{
+  public Guid Id { get; set; }
+  public required string Name { get; set; }
+  public string? Description { get; set; }
+  public bool IsComplete { get; set; }
+
+}
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TodoContext") ?? throw new InvalidOperationException("Connection string 'TodoContext' not found.");
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 
 var app = builder.Build();
 
